31fce80a7080e89484286434cdcd4e84a8966e4f
Lukas Kouril <kouril.lukas@gmail.com>
2024-11-01 18:04:33 +0100

    FE on the way

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const BASE_URL = "http://localhost:8080";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [newEntry, setNewEntries] = useState([]);
+  const [entry, setEntry] = useState([]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await fetch(`${BASE_URL}/entries`)
+      .then((res) => res.json())
+      .then((entry) => {
+        setNewEntries(entry);
+        setNewEntries("");
+      });
+  }
+
+  async function getEntry() {
+    const response = await fetch(`${BASE_URL}/entries`);
+    const entry = await response.json();
+    setEntry(entry);
+  }
+
+  useEffect(() => {
+    getEntry();
+  }, []);
+
+  // setInterval()
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Home</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="input"
+          placeholder="Write something.."
+        ></input>
+        <button>Post</button>
+      </form>
+      <div className="post_stack">
+        {entry &&
+          entry.map((entry, index) => (
+            <div key={index}>
+              <p>{entry.author}</p>
+              <p>{entry.text}</p>
+            </div>
+          ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
